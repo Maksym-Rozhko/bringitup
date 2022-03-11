4effe3b9ece9f23943bc1a205639671cd47dc61c
Maksym <maksymRozhko7@gmail.com>
2022-03-11 20:47:26 +0200

    feat: play video in modal, youtube API

--- dist/script.js
+++ dist/script.js
@@ -1845,11 +1845,76 @@ module.exports = g;
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_slider__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/slider */ "./src/js/modules/slider.js");
+/* harmony import */ var _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/playVideo */ "./src/js/modules/playVideo.js");
+
 
 window.addEventListener('DOMContentLoaded', () => {
   const mainSlider = new _modules_slider__WEBPACK_IMPORTED_MODULE_0__["default"]('.page', '.next', 'slideInUp');
   mainSlider.render();
+  const player = new _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__["default"]('.showup .play', '.overlay');
+  player.init();
 });
+
+/***/ }),
+
+/***/ "./src/js/modules/playVideo.js":
+/*!*************************************!*\
+  !*** ./src/js/modules/playVideo.js ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return VideoPlayer; });
+class VideoPlayer {
+  constructor(triggers, overlay) {
+    this.btns = document.querySelectorAll(triggers);
+    this.overlay = document.querySelector(overlay);
+    this.close = this.overlay.querySelector('.close');
+  }
+
+  bindTriggers() {
+    this.btns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        if (document.querySelector('iframe#frame')) {
+          this.overlay.classList.add('show-flex');
+        } else {
+          const path = btn.dataset.url;
+          this.createPlayer(path);
+        }
+
+        ;
+      });
+    });
+  }
+
+  bindCloseBtn() {
+    this.close.addEventListener('click', () => {
+      this.overlay.classList.remove('show-flex');
+      this.player.pauseVideo();
+    });
+  }
+
+  createPlayer(url) {
+    this.player = new YT.Player('frame', {
+      height: '100%',
+      width: '100%',
+      videoId: `${url}`
+    });
+    this.overlay.classList.add('show-flex');
+  }
+
+  init() {
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    this.bindTriggers();
+    this.bindCloseBtn();
+  }
+
+}
 
 /***/ }),
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from "./modules/slider";
+import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
     const mainSlider = new Slider('.page', '.next', 'slideInUp');
     mainSlider.render();
+
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 });
